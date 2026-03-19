9c3f30a0b8c4a2288a09ea45cd3808977e6fe79e
Harry Rinaudo <hfr3127@g.rit.edu>
2026-03-19 17:12:05 -0400

    BERT start

--- code/BERT_Classification.py
+++ code/BERT_Classification.py
@@ -1,11 +1,13 @@
-import pandas as pd
-import numpy as np
+import torch
+from transformers import pipeline, BertTokenizer
 import preprocessing
-
-
-
 df, tfidf_matrix, feature_names = preprocessing.preprocess_data()
+tweets = df["tweet"]
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 
-print(df)
-print(tfidf_matrix)
-print(feature_names)
+print("start")
+tokenized_tweets = [tokenizer.tokenize(tweet) for tweet in tweets]
+
+
+print(tweets)
+print("done")
