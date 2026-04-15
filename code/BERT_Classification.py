87cb2320cd864cfd559c24c40032fcb4bdf9add3
Harry Rinaudo <hfr3127@g.rit.edu>
2026-04-15 15:21:18 -0400

    BERT output update

--- code/BERT_Classification.py
+++ code/BERT_Classification.py
@@ -1,10 +1,13 @@
 import torch
 from transformers import pipeline
+from transformers import BertConfig, BertModel
 import preprocessing
 df, tfidf_matrix, feature_names = preprocessing.preprocess_data()
 tweets = df["tweet"]
 sentiments = df["sentiment"]
-
+configuration = BertConfig()
+model = BertModel(configuration)
+configuration = model.config
 sentiment_analyzer = pipeline(
     "sentiment-analysis",
     model="distilbert-base-uncased-finetuned-sst-2-english"
@@ -59,22 +62,30 @@ for tweet, sentiment in zip(tweets, sentiments):
     
 
 accuracy = accuracy / count
-print("Accuracy: %d", accuracy)
+print("Accuracy: ", accuracy)
 precisionP = TP/(TP + FP)
 recallP = TP/P
+f1P = 2*(precisionP*recallP)/(precisionP+recallP)
 precisionNT = TNT/(TNT + FNT)
 recallNT = TNT/NT
+f1NT = 2*(precisionNT*recallNT)/(precisionNT+recallNT)
 precisionNG = TNG/(TNG + FNG)
 recallNG = TNG/NG
+f1NG = 2*(precisionNG*recallNG)/(precisionNG+recallNG)
 averagePrecision = (precisionNG + precisionNT + precisionP)/3
 averageRecal = (recallNG + recallNT + recallP)/3
+averageF1 = 2*(averagePrecision*averageRecal)/(averagePrecision+averageRecal)
 print("Positive precision: ", precisionP)
 print("Positive recall: ", recallP)
+print("Positive F1: ", f1P)
 print("Neutral precision: ", precisionNT)
 print("Neutral recall: ", recallNT)
+print("Neutral F1: ", f1NT)
 print("Negative precision: ", precisionNG)
 print("Negative recall: ", recallNG)
+print("Negative F1: ", f1NG)
 print("Average precision: ", averagePrecision)
 print("Average recall: ", averageRecal)
+print("Average F1: ", averageF1)
 
 #accuracy, precision, recall, f1
