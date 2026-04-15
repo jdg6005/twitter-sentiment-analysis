c2c4f736717aa6bbcac7f8c66dfe7bb868d6fba6
Jaidan Giglio <jdg6005@rit.edu>
2026-04-15 15:35:46 -0400

    preprocessing + naive bayes update

--- code/Naive_Bayes.py
+++ code/Naive_Bayes.py
@@ -1,17 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.utils.multiclass import unique_labels
 from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
 import preprocessing
 
 def main():
-    df, _, _ = preprocessing.preprocess_data()
+    df, tt_split = preprocessing.preprocess_data()
     
-    X = df["tweet"]
-    y = df['sentiment']
-    X_train_full, X_test_full, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=35, stratify=y)
+    X_train_full, X_test_full, y_train, y_test = tt_split
 
     vector = TfidfVectorizer(ngram_range=(2, 2))
     X_train = vector.fit_transform(X_train_full)
@@ -32,6 +31,9 @@ def main():
 
     # confusion matrix pandas dataframe
     confuseDF = pd.DataFrame(confuseM, index=[f"Actual {label}" for label in confuseL], columns=[f"Predicted {label}" for label in confuseL])
+
+    confuseDisp = ConfusionMatrixDisplay(confuseM, display_labels=confuseL).plot(cmap=plt.cm.Blues)
+    plt.show()
     
     print(f"Accuracy: {accuracy:.4f}")
     print(f"F1 Score: {F1_score:.4f}")
--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
 
 
@@ -9,13 +10,13 @@ def preprocess_data():
     df = pd.read_csv("./data/twitter_training.csv", header=None) # path to data
     df.columns = ["id", "entity", "sentiment", "tweet"] # naming columns for easier access
     df = df.dropna()
+    
+    X = df["tweet"]
+    y = df['sentiment']
 
-    vectorizer = TfidfVectorizer(ngram_range=(2, 2))  
-    tfidf_matrix = vectorizer.fit_transform(df['tweet'])
+    tt_split = train_test_split(X, y, test_size=0.2, random_state=35, stratify=y)
 
-    feature_names = vectorizer.get_feature_names_out()
-    print("Feature Names:", feature_names)
-    return df, tfidf_matrix, feature_names
+    return df, tt_split
 
 
 
